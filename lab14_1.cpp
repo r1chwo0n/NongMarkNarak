76502f8520af9c4f315ac10110295b7c9c45f53e
r1chwo0n <woonzy09@gmail.com>
2023-01-13 16:37:15 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,29 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat (const double x[], int y, double z[]){
+    double sum = 0;
+    for (int i = 0; i < y; i++) sum += x[i];
+    z[0] = sum / y;
+    
+    double sum1 = 0;
+    for (int i = 0; i < y; i++) sum1 += pow(x[i],2);
+    z[1] = sqrt( (sum1/y) - pow(z[0],2));
+    
+    double sum2 = 1;
+    for (int i = 0; i < y; i++) sum2 *= x[i];
+    z[2] = pow(sum2, 1.0/y);
+    
+    double sum3 = 0;
+    for (int i = 0; i < y; i++) sum3 += 1.0/x[i];
+    z[3] = y / sum3;
+ 
+    double max = x[0],min = x[0];
+    for (int i = 0; i < y; i++){
+        if (x[i] > max) max = x[i];
+        if (x[i] < min) min = x[i];
+    }
+    z[4] = max;
+    z[5] = min;
+}
